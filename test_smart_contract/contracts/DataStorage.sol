21eeaaf85920d0fb972d533fb01853044caae9cd
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2025-01-27 07:34:45 +0200

    3 change name of field and add one new
    dataStorage -> balance
    +address owner

--- test_smart_contract/contracts/DataStorage.sol
+++ test_smart_contract/contracts/DataStorage.sol
@@ -3,17 +3,23 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract DataStorage {
 
-  uint256 public dataStorage;
+  uint256 private balance;
+  address private owner;
 
+  constructor() {
+    owner = msg.sender;
+  }
 
-  // constructor() public {
-  // }
-
-  function setData(uint256 x) public {
-    dataStorage = x;
+  function setData(uint256 _balance) public {
+    require(_balance > 0, "Error! The balance have to be > 0.");
+    balance = _balance;
   }
 
   function getData() public view returns (uint256){
-    return dataStorage;
+    return balance;
+  }
+
+  function getOwner() public view returns (address){
+    return owner;
   }
 }
